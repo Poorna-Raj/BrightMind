ed06a86531e228968bfeb943eea513f26545876b
Poorna <poornarajapaksha339@gamil.com>
2025-06-06 00:26:49 +0530

    Create basic project structure

---
+++ src/main/java/com/mycompany/brightmind/BrightMind.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.brightmind;
+
+/**
+ *
+ * @author Poorna
+ */
+public class BrightMind {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
